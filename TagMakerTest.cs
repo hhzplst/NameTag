250c802c2cd2846fb11ad285c2d7f706af8899a0
Ivy Chang <ivycwq@icloud.com>
2016-09-09 10:39:50 -0700

    adding test class

---
+++ TagMakerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TagMaker {
+    public class TagMakerTest {
+        public static void Main (string[] args) {
+            TagMaker testTagMaker = new TagMaker();
+            testTagMaker.PrintTag();
+
+            testTagMaker.Name = "Ivy";
+            testTagMaker.Organization = "Berkeley City College";
+            testTagMaker.PrintTag();
+
+            testTagMaker.PrintBlankTag();
+            testTagMaker.PrintTag();
+
+            testTagMaker.ClearSettings();
+            testTagMaker.PrintTag(); 
+        }
+    }
+}
